476d672e870f28949864407e56814588d0c3301f
OneDeMillion <knittinbrit@gmail.com>
2022-10-02 17:53:31 -0500

    change route on index

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require("express"); // pulls in express library
 const bodyParser = require('body-parser'); // 
 const app = express(); // use to configure our server
 const mongodb = require('./initializers/db'); // 
-const routes = require("./routes");
+// const routes = require("./routes");
 const port = process.env.PORT || 8080;
 
 
@@ -15,11 +15,11 @@ app
         res.setHeader('Access-Control-Allow-Origin', '*');
         next();
     })
-    .use(routes);
+    .use('/', require('./routes'));
 
 mongodb.initDb((err, mongodb) => {
     if (err) {
-        console.log({ERROR: ', err'});
+        console.log(err);
     } else {
         app.listen(port);
         console.log(`Connected to DB. Listening on http://localhost:${port}`);

e76a46d34ae913b475ce9088609833275b4518b6
OneDeMillion <knittinbrit@gmail.com>
2022-10-06 19:52:25 -0500

    eslintrc adjustment node

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,10 +1,10 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
 const contactsController = require('../controllers/contacts');
 
 // gets all contacts in collection
-router.get('/', contactsController.getAll)
+router.get('/', contactsController.getAll);
 // gets contact identified by id endpoint
 router.get('/:id', contactsController.getOne);
 // creates a new contact
